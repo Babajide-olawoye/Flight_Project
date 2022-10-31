558fc9a5d14c750db0743422ed68b1d617f6dad0
Babajide Oke <josua.oke@solera.com>
2022-10-31 19:45:08 +0000

    input boxes in home pg

--- airline_prj_frontend/src/Components/Home.js
+++ airline_prj_frontend/src/Components/Home.js
@@ -1,9 +1,32 @@
+import Date from "./Date";
+
 export default function Home() {
-    return (
-      <div>
-        <h1>Home!</h1>
-      </div>
-    );
-  }
-  
-  
+  return (
+    <div align= 'center'>
+      <form>
+        <div>
+          <label>Departure from:</label>
+        </div>
+        <div>
+          <input/>
+        </div>
+        <div>
+          <label>Arriving at:</label>
+        </div>
+        <div>
+          <select>
+          <option value="none" selected disabled hidden>Select available arriaval options</option>
+          </select>
+        </div>
+        <div>
+          <Date/>
+        </div>
+        
+       
+      </form>
+
+    </div>
+
+  );
+}
+
