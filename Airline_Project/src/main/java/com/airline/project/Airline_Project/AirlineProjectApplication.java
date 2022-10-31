229d5d9909f026a9a975de02a5b7a94e84853eb8
Kirylo <dainius.kirylo@solera.com>
2022-10-31 15:13:03 +0000

    Initial spring boot application

---
+++ Airline_Project/src/main/java/com/airline/project/Airline_Project/AirlineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.airline.project.Airline_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineProjectApplication.class, args);
+	}
+
+}
